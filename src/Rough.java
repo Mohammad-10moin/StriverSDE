06f9d653e8109280fc774550b5b8b1704882a860
Mohammad-10moin <moinuddin10code@gmail.com>
2024-07-20 18:58:28 +0530

    rough file

--- src/Rough.java
+++ src/Rough.java
@@ -1,17 +1,31 @@
-import java.util.Arrays;
-
+import java.util.*;
 public class Rough {
+    public static void swap(int[]a,int s,int e){
+        int temp=a[s];
+        a[s]=a[e];
+        a[e]=temp;
+    }
     public static void main(String[] args) {
-        int [] nums={3,2,1};
-        for(int m=0;m<nums.length-1;m++){
-            for(int j=0;j< nums.length-m-1;j++){
-                if(nums[j]>nums[j+1]){
-                    int dupe=nums[j];
-                    nums[j]=nums[j+1];
-                    nums[j+1]=dupe;
-                }
+        String s="45320";
+        int n= Integer.valueOf(s);
+        System.out.println(n);
+        int index=-1;
+        int []num = new int[s.length()];
+        int k=0;
+        for(int j=n;j>0;j=j/10){
+            num[num.length-1-k]=(j%10);
+            k++;
+        }
+        System.out.println(Arrays.toString(num));
+        for(int i=num.length-1;i>0;i--){
+            if(num[i]>num[i-1]){
+                index=i;
+                break;
             }
         }
-        System.out.println(Arrays.toString(nums));
+        System.out.println(index);
+        System.out.println(index+1);
+        swap(num,index,index+1);
+        System.out.println(Arrays.toString(num));
     }
 }
